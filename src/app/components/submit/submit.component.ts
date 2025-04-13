7ac5637f9d6d14584360c44b947032525a60977d
hugo <hugomdmg@gmail.com>
2025-04-13 14:09:36 +0200

    1.0.0

--- src/app/components/submit/submit.component.ts
+++ src/app/components/submit/submit.component.ts
@@ -14,6 +14,8 @@ export class SubmitComponent implements OnInit {
   signature: string | null = null;
   creating = false
   name = ""
+  show_alert = false
+  show_alert_name = false
   date = new Date().toISOString().split('T')[0]
 
   constructor(private questionsService: QuestionsService) { }
@@ -34,21 +36,42 @@ export class SubmitComponent implements OnInit {
   }
 
   async generatePDFfromHTML() {
-    this.creating = true
-    const element: any = document.getElementById('pdf-content');
-    if (!element) { return; }
+    if(this.checkComplet()){
+      this.creating = true
+      const element: any = document.getElementById('pdf-content');
+      if (!element) { return; }
+  
+      const canvas = await html2canvas(element, { scale: 3, logging: true });
+      const imgData = canvas.toDataURL('image/png');
+  
+      const pdf = new jsPDF();
+      const imgProps = pdf.getImageProperties(imgData);
+      const pdfWidth = pdf.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+  
+      pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+  
+      pdf.save('medical_form.pdf');
+      this.creating = false
+    }
+  }
 
-    const canvas = await html2canvas(element, { scale: 3, logging: true });
-    const imgData = canvas.toDataURL('image/png');
-
-    const pdf = new jsPDF();
-    const imgProps = pdf.getImageProperties(imgData);
-    const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-
-    pdf.save('medical_form.pdf');
-    this.creating = false
+  checkComplet():boolean{
+    let response = true
+    this.questions.forEach((element: any) => {
+      if(element.answer === undefined){
+        this.show_alert = true
+        response = false
+      }else{
+        this.show_alert = false
+      }
+    });
+    if(!this.name){
+      response = false
+      this.show_alert_name = true
+    }else{
+      this.show_alert_name = false
+    }
+    return response
   }
 }
