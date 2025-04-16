4cee2152472238d8bda72f327a1b23bd6ae647df
hugo <hugomdmg@gmail.com>
2025-04-16 18:56:31 +0200

    safe working

--- src/app/components/safe/safe.component.ts
+++ src/app/components/safe/safe.component.ts
@@ -7,25 +7,14 @@ import { QuestionsService } from 'src/app/services/questions.service';
   styleUrls: ['./safe.component.css']
 })
 export class SafeComponent {
-  questions: any = undefined
+  questions = []
   name = ""
 
   constructor(private questionsService: QuestionsService) { }
 
-  ngOnInit(): void {
-    this.questionsService.questions$.subscribe(questions => {
-      if (questions) { this.questions = questions }
-    })
-    this.questionsService.name$.subscribe(name => { this.name = name })
-  }
-
   updateName(event: Event) {
     const name = (event.target as HTMLInputElement).value
     this.questionsService.updateName(name)
-  }
-
-  updateAnswer(name: any, answer: any) {
-    this.questionsService.updateAnswer(name, answer)
   }
 
 }
